6d8e335e511df4efd74308e99d168f3fdd6dea25
Mino <tladus@gmail.com>
2018-03-15 00:13:54 +0900

    -Chapter 2&3

---
+++ codes/chapter3/drawstepfun.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_func(x):
+    return np.array(x > 0, dtype = np.int)
+
+def draw_graph():
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_func(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+
+if __name__ == '__main__':
+    draw_graph()
